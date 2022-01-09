eb446a17691aff65dffeef9bcffb4d85f54a4178
Pushkar sabe <pushkarsabe@gmail.com>
2022-01-09 10:29:15 +0530

    new commit

--- Java-jenkins-in-docker/src/main/java/org/example/Java_jenkins_in_docker/App.java
+++ Java-jenkins-in-docker/src/main/java/org/example/Java_jenkins_in_docker/App.java
@@ -7,7 +7,7 @@ package org.example.Java_jenkins_in_docker;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! HI there");
 	}
 
 	public static boolean checkIfInputIsAnEvenNumber(int number) {
